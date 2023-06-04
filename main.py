a7e2c1046051a8c803c38836176df6e8273ac39e
ArMaGeDDoN-SS <120749588+ArMaGeDDoN-SS@users.noreply.github.com>
2023-06-04 19:41:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -22,17 +22,10 @@ intents.members = True
 client = commands.Bot(command_prefix=prefix, intents=intents)
 client.remove_command('help')
 
-@client.command()
-async def spam_send(ctx, count):
-    try:
-        with open(spam_text_file, 'r') as spam_text_n:
-            spam_text = spam_text_n.read()
-        await ctx.send(spam_text.strip('\"'))
-    except Exception as e: print(e)
 
 @client.command()
 async def load(ctx, extension):
-    if ctx.author.id in developers_list:
+    if ctx.author.id in developer_list:
         client.load_extension(f"cogs {extension}")
         await ctx.send("Cogs loaded!")
     else:
@@ -41,7 +34,7 @@ async def load(ctx, extension):
 
 @client.command()
 async def unload(ctx, extension):
-    if ctx.author.id in developers_list:
+    if ctx.author.id in developer_list:
         client.unload_extension(f"cogs {extension}")
         await ctx.send("Cogs unloaded!")
     else:
@@ -50,7 +43,7 @@ async def unload(ctx, extension):
 
 @client.command()
 async def reload(ctx, extension):
-    if ctx.author.id in developers_list:
+    if ctx.author.id in developer_list:
         client.unload_extension(f"cogs {extension}")
         client.load_extension(f"cogs {extension}")
         await ctx.send("Cogs reloaded!")
